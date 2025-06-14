48082f1572d666fdafe6392c94179b50de1643cd
MiltonJR7 <milton.oliveira.junior777@gmail.com>
2025-06-14 14:24:11 -0300

    API Node com Postgres

---
+++ server.js
@@ -0,0 +1,51 @@
+import { fastify } from 'fastify'
+import { DatabasePostgres } from './database-postgres.js';
+// import { DatabaseMemory } from './database-memory.js'
+
+const server = fastify();
+const database = new DatabasePostgres()
+// const database = new DatabaseMemory()
+
+
+server.post('/videos', async (request, reply) => {
+    const { title, description, duration } = request.body
+
+    await database.create({
+        title: title,
+        description: description,
+        duration: duration,
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', async (request, reply) => {
+    const search = request.query.search
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+})
